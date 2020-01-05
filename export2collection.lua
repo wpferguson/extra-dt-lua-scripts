1e9aa5603ead535e4af21c475a49f70e69926350
Bill Ferguson <wpferguson@gmail.com>
2020-01-05 00:18:15 -0500

    removed old code and replaced it with library calls.

--- export2collection.lua
+++ export2collection.lua
@@ -43,6 +43,7 @@
 
 local dt = require "darktable"
 local du = require "lib/dtutils"
+local df = require "lib/dtutils.file"
 local gettext = dt.gettext
 
 du.check_min_api_version("3.0.0", "export2collection")
@@ -54,96 +55,9 @@ local function _(msgid)
     return gettext.dgettext("export2collection", msgid)
 end
 
-local function split_filepath(str)
-  local result = {}
-  -- strip out single quotes from quoted pathnames
-  str = string.gsub(str, "'", "")
-  -- Thank you Tobias Jakobs for the awesome regular expression, which I tweaked a little
-  result["path"], result["filename"], result["basename"], result["filetype"] = string.match(str, "(.-)(([^\\/]-)%.?([^%.\\/]*))$")
-  return result
-end
-
-local function get_path(str)
-  local parts = split_filepath(str)
-  return parts["path"]
-end
-
-local function get_filename(str)
-  local parts = split_filepath(str)
-  return parts["filename"]
-end
-
-local function get_basename(str)
-  local parts = split_filepath(str)
-  return parts["basename"]
-end
-
-local function get_filetype(str)
-  local parts = split_filepath(str)
-  return parts["filetype"]
-end
-
-function check_if_bin_exists(bin)
-  local result = os.execute("which " .. bin)
-  if not result then
-    result = false
-  end
-  return result
-end
-
--- Thanks Tobias Jakobs for the idea 
-function check_if_file_exists(filepath)
-  local result = os.execute("test -e " .. filepath)
-  if not result then
-    result = false
-  end
-  return result
-end
-
-local function filename_increment(filepath)
-
-  -- break up the filepath into parts
-  local path = get_path(filepath)
-  local basename = get_basename(filepath)
-  local filetype = get_filetype(filepath)
-
-  -- check to see if we've incremented before
-  local increment = string.match(basename, "_(%d-)$")
-
-  if increment then
-    -- we do 2 digit increments so make sure we didn't grab part of the filename
-    if string.len(increment) > 2 then
-      -- we got the filename so set the increment to 01
-      increment = "01"
-    else
-      increment = string.format("%02d", tonumber(increment) + 1)
-      basename = string.gsub(basename, "_(%d-)$", "")
-    end
-  else
-    increment = "01"
-  end
-  local incremented_filepath = path .. basename .. "_" .. increment .. "." .. filetype
-
-  dt.print_error("original file was " .. filepath)
-  dt.print_error("incremented file is " .. incremented_filepath)
-
-  return incremented_filepath
-end
-
 local function show_status(storage, image, format, filename,
   number, total, high_quality, extra_data)
     dt.print(string.format(_("Export Image %i/%i"), number, total))
-end
-
-local function file_move(fromFile, toFile)
-  local success = os.rename(fromFile, toFile)
-  if not success then
-    -- an error occurred, so let's try using the operating system function
-    if checkIfBinExists("mv") then
-      success = os.execute("mv '" .. fromFile .. "' '" .. toFile .. "'")
-    end
-  end
-  return success  -- nil on error, some value if success
 end
 
 local function import_exports(storage, image_table, extra_data) --finalize
@@ -155,28 +69,28 @@ local function import_exports(storage, image_table, extra_data) --finalize
 
   for image,exported_image in pairs(image_table) do
 
-    local myimage_name = image.path .. "/" .. get_filename(exported_image)
+    local myimage_name = image.path .. "/" .. df.get_filename(exported_image)
 
-    while check_if_file_exists(myimage_name) do
-      myimage_name = filename_increment(myimage_name)
+    while df.check_if_file_exists(myimage_name) do
+      myimage_name = df.filename_increment(myimage_name)
       -- limit to 99 more exports of the original export
-      if string.match(get_basename(myimage_name), "_(d-)$") == "99" then 
+      if string.match(df.get_basename(myimage_name), "_(d-)$") == "99" then 
         break 
       end
     end
 
-    dt.print_error("moving " .. exported_image .. " to " .. myimage_name)
-    local result = file_move(exported_image, myimage_name)
+    dt.print_log("moving " .. exported_image .. " to " .. myimage_name)
+    local result = df.file_move(exported_image, myimage_name)
 
     if result then
-      dt.print_error("importing file")
+      dt.print_log("importing file")
       local myimage = dt.database.import(myimage_name)
 
       myimage:group_with(image.group_leader)
 
       for _,tag in pairs(dt.tags.get_tags(image)) do 
         if not (string.sub(tag.name,1,9) == "darktable") then
-          dt.print_error("attaching tag")
+          dt.print_log("attaching tag")
           dt.tags.attach(tag,myimage)
         end
       end
